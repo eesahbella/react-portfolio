d37081750c1f748de7f333fb2d43a54c99dd606f
bella <isakawaii365@gmail.com>
2024-03-07 20:14:33 +0000

    fixes navbar

--- src/components/Home/Navbar.jsx
+++ src/components/Home/Navbar.jsx
@@ -2,20 +2,19 @@ import React from "react";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { Link } from "react-router-dom";
 
 const NavBar = () => {
     return (
         <Navbar expand="lg" className="nav-bar">
             <Container fluid>
-                <Navbar.Brand className="text-light me-auto" as={Link} to="/">ITMC</Navbar.Brand>
+                <Navbar.Brand className="text-light me-auto" href="#home">ITMC</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
                     <Nav>
-                        <Nav.Link className="text-light me-3" as={Link} to="/hero">Home</Nav.Link>
-                        <Nav.Link className="text-light me-3" as={Link} to="/skills">My Skills</Nav.Link>
-                        <Nav.Link className="text-light me-3" as={Link} to="/projects">My Projects</Nav.Link>
-                        <Nav.Link className="text-light me-3" as={Link} to="/contact">Contact Me</Nav.Link>
+                        <Nav.Link className="text-light me-3" href="#heroSection">Home</Nav.Link>
+                        <Nav.Link className="text-light me-3" href="#mySkills">My Skills</Nav.Link>
+                        <Nav.Link className="text-light me-3" href="#myProjects">My Projects</Nav.Link>
+                        <Nav.Link className="text-light me-3" href="#Contact">Contact Me</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
